f9a8a28aba533f356c7926652173edebf4f98fda
efabapo <fabio.apolonio@ericsson.com>
2017-11-14 17:17:37 +0000

    first deploy

---
+++ ejbexpert/src/main/java/com/company/TestEjb111.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.ejb.Stateless;
+
+/**
+ * Created by efabapo on 14/11/17.
+ */
+@Stateless
+public class TestEjb111 {
+    public String test111(){
+        return "Test111";
+    }
+}
